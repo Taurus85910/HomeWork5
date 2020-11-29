cf887efdee2205e8b1d76863e9a5fcc52d7ff1fb
Kostya <96919923@mail.ru>
2020-11-29 17:15:54 +1000

    6

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _entry;
+    [SerializeField] private Door _door;
+    private AudioSource _audio;
+    private bool _isAlarmStarted;
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+        _audio.volume = 0;
+        _isAlarmStarted = false;
+    }
+
+    void Update()
+    {
+        if (!_isAlarmStarted && _door.CheckAlarm())
+        {
+            _entry?.Invoke();
+            _isAlarmStarted = true;
+        }
+        if (_door.CheckAlarm())
+        {           
+            _audio.volume += 0.001f;
+        }
+        else
+        {
+            _audio.volume -= 0.01f;
+        }
+        if (!_door.CheckAlarm() && _audio.volume == 0)
+            _audio.Stop();
+    }
+}
--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,53 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 
 public class Door : MonoBehaviour
 {
 
-    [SerializeField] private UnityEvent _entry;
-    private bool _isRobberInHouse;
-    private bool _isRobberWasInHouse;
     private SpriteRenderer _door;
-    private AudioSource _audio;
-
-
-    private void Start()
-    {
-        _door = GetComponent<SpriteRenderer>();
-        _audio = GetComponent<AudioSource>();
-        _audio.volume = 0;
-    }
-
+    private bool _isAlarmOn;
+ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
         if (collision.TryGetComponent<Robber>(out Robber robber))
         {
-            if (!_isRobberInHouse)
-            {
-                _door.color = Color.red;
-                _isRobberInHouse = true;
-                _isRobberWasInHouse = true;
-                _entry?.Invoke();
-            }
-            else
-            {
-                _door.color = Color.green;
-                _isRobberInHouse = false;
-            }
+            if (!_isAlarmOn)          
+                _isAlarmOn = true;                        
+            else            
+                _isAlarmOn = false;           
         }
     }
-
-    private void Update()
-    {
-        if (_isRobberInHouse)
-            _audio.volume += 0.001f;
-        else
-            _audio.volume -= 0.01f;
-        if (_isRobberWasInHouse && !_isRobberInHouse && _audio.volume == 0)
-            _audio.Stop();
-    }
+    public bool CheckAlarm() => _isAlarmOn;
 }
